bd4f86d05255a2abcdfcb152f53082cdbbf2d70e
Vladimir <magicpilya@yandex.ru>
2023-03-06 16:15:59 +0300

    added form interaction

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import modals from "./modules/modals";
 import sliders from "./modules/sliders";
+import forms from "./modules/forms";
 
 window.addEventListener('DOMContentLoaded', () => {
 
@@ -8,5 +9,6 @@ window.addEventListener('DOMContentLoaded', () => {
     modals();
     sliders('.feedback-slider-item', 'horizontal', 3000, '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical', 3500);
+    forms();
 
 });

3a863deba09541c04796971f49b2a0ef2ab7f5e7
Vladimir <magicpilya@yandex.ru>
2023-03-06 17:14:31 +0300

    added phone-validation

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import modals from "./modules/modals";
 import sliders from "./modules/sliders";
 import forms from "./modules/forms";
+import mask from "./modules/mask";
+import checkTextInputs from "./modules/checkTextInputs";
 
 window.addEventListener('DOMContentLoaded', () => {
 
@@ -10,5 +12,8 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider-item', 'horizontal', 3000, '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical', 3500);
     forms();
+    mask("[name='phone']");
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
 
 });
